bde8ec7d4d08e48203aa83db7da241225098c9dd
bekarysalashybayev <bekarysalashybayev@gmail.com>
2022-12-18 18:34:58 +0600

    name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'TestEntAdminDev',
+      name: process.env.APP_NAME,
       exec_mode: 'cluster',
       instances: 'max',
       script: './node_modules/nuxt/bin/nuxt.js',
